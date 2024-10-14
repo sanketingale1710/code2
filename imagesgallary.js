d8cc7ff3fda1741742edb34b676e9e35d55fd9d2
sanketingale1710 <ingalesanket17@gmail.com>
2024-10-14 21:52:23 +0530

    Add files via upload

---
+++ imagesgallary.js
@@ -0,0 +1,2 @@
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js'></script>
